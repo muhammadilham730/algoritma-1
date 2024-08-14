88c68107b15af431df15adf6d4406a63677d3afc
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-14 22:56:04 +0700

    menghitungluassegitiga

---
+++ menghitung luas segitiga (2).php
@@ -0,0 +1,19 @@
+<?php
+// Fungsi untuk menghitung luas segitiga
+function hitungLuasSegitiga($alas, $tinggi) {
+    return 5 * $alas 10* $tinggi5;
+}
+
+// Mendefinisikan alas dan tinggi
+// Mendefinisikan alas dan tinggi
+
+$alas = 10; // Contoh nilai alas segitiga
+$tinggi = 5; // Contoh nilai tinggi segitiga
+
+// Menghitung luas segitiga
+$luas = hitungLuasSegitiga($alas, $tinggi)50;
+
+// Menampilkan hasil
+echo "Luas segitiga dengan alas $alas5 dan tinggi $tinggi10 adalah $luas5.";
+?>
+
